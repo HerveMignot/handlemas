e5fd2e9aad3244a90b6febe8718d12884ed53a23
Hervé MIGNOT <MIGNOT@equancy.com>
2021-11-25 22:00:00 +0100

    Add estimation through multiple runs

--- app/app.py
+++ app/app.py
@@ -2,10 +2,14 @@ import random
 
 import streamlit as st
 
+from typing import Tuple, List
+
+from streamlit.config import _global_development_mode
+
 
 MAX_GAMES = 10_000
 
-def simulate_handle(start: float, payback_rate: float, bet: float, run: int = 100) -> float:
+def simulate_handle(start: float, payback_rate: float, bet: float) -> Tuple[float, int, List[float]]:
     """Run a simulation game and stop when player is broken
 
     Parameters
@@ -32,15 +36,36 @@ def simulate_handle(start: float, payback_rate: float, bet: float, run: int = 10
     return (handle, games, history)
 
 
+def estimate_handle(start: float, payback_rate: float, bet: float, run: int = 100) -> Tuple[float, float, List[float], List[int]]:
+    handles: List[float] = []
+    games: List[int] = []
+
+    for r in range(run):
+        handle, game, _ = simulate_handle(start, payback_rate, bet)
+        handles.append(handle)
+        games.append(game)
+    return (sum(handles) / len(handles), sum(games) / len(games), handles, games)
+
+    
 st.title("Handle Estimator")
 st.write("Try with initial 100, 94% and minimum 3")
 start_amount = st.number_input('Initial amount:', min_value=0, max_value=1000)
 payback_rate = st.number_input('Payback rate:', min_value=0.0, max_value=1.0, value=0.95)
 minimum_bet = st.number_input('Minimum bet:', min_value=1, max_value=10)
 
-simulate_btn = st.button("Simulate 1 game")
+buttons = st.columns(2)
+simulate_btn = buttons[0].button("Simulate 1 game")
+estimate_btn = buttons[1].button("Estimate handle")
+
 if simulate_btn:
     handle, games, history = simulate_handle(start_amount, payback_rate, minimum_bet)
-    st.write('Estimated Handle:', handle)
+    st.write('Computed handle:', handle)
     st.write('Number of games', games)
     st.line_chart(history)
+
+if estimate_btn:
+    nb_runs = 100
+    estimated_handle, average_games, handles, games = estimate_handle(start_amount, payback_rate, minimum_bet, nb_runs)
+    st.write('Estimated handle:', estimated_handle, ' over ', nb_runs, 'simulation')
+    st.write('Average number of games', average_games)
+    st.line_chart(handles)
