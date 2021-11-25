c8cf2f32b913e6daa1e9d98ca435558ae8275689
HerveMignot <MIGNOT@equancy.com>
2021-11-25 22:31:59 +0100

    Add slider for number of runs

--- app/app.py
+++ app/app.py
@@ -53,9 +53,10 @@ start_amount = st.number_input('Initial amount:', min_value=0, max_value=1000)
 payback_rate = st.number_input('Payback rate:', min_value=0.0, max_value=1.0, value=0.95)
 minimum_bet = st.number_input('Minimum bet:', min_value=1, max_value=10)
 
-buttons = st.columns(2)
+buttons = st.columns(3)
 simulate_btn = buttons[0].button("Simulate 1 game")
 estimate_btn = buttons[1].button("Estimate handle")
+nb_runs = buttons[2].slider('Runs:', 0, 500, 100, step=10)
 
 if simulate_btn:
     handle, games, history = simulate_handle(start_amount, payback_rate, minimum_bet)
@@ -63,9 +64,9 @@ if simulate_btn:
     st.write('Number of games', games)
     st.line_chart(history)
 
-if estimate_btn:
-    nb_runs = 100
+if estimate_btn and nb_runs > 0:
+    #nb_runs = 100
     estimated_handle, average_games, handles, games = estimate_handle(start_amount, payback_rate, minimum_bet, nb_runs)
-    st.write('Estimated handle:', estimated_handle, ' over ', nb_runs, 'simulation')
-    st.write('Average number of games', average_games)
+    st.write('Estimated handle:', round(estimated_handle, 2), ' over ', nb_runs, 'simulation')
+    st.write('Average number of games', round(average_games,1))
     st.line_chart(handles)
